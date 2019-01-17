8f5c8c6b1491384ba7f16cbb0b47f67ac107a9f9
Mario Serrano <maseflores79@gmail.com>
2019-01-17 14:39:11 -0800

    agregado pipe json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,14 @@ export class AppComponent {
   arreglo= [1,2,3,4,5,6,7,8,9,10];
   PI = Math.PI;
   a = 0.234;
-  salario= 1234.5
+  salario= 1234.5;
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "primera",
+      numero: "10"
+    }
+  };
 }
